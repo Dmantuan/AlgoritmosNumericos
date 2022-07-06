77ba18b657442942d2bed6fd17c44e176c1c00d7
Dmantuan <danielgmantuan@gmail.com>
2022-07-06 15:39:41 -0300

    Eliminacao de Gauss

--- Trabalho1.c
+++ Trabalho1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-float* Gauss(float **A, float *B, int N);
+float* Gauss(float **A, int N);
 
 void main(){
     FILE *file;
@@ -24,24 +24,34 @@ void main(){
         */
        
         //inicializacao e implementacao da matriz 
-        float M[dimension+1][dimension+1];
-        char numeros[10];
+        float **M;
+        float *X;
+        X = (float *) malloc(dimension*sizeof(int));
+        M = (float **) malloc(dimension*sizeof(int));
         for (int i = 0; i < dimension; i++){
+            M[i] = (float*)malloc((dimension+1)*sizeof(int*));
             for (int j = 0; j < dimension; j++){
                 fscanf(file,"%f", &M[i][j]);
             }
         }
-        for (int i = 0; i < dimension; i++){
-            fscanf(file,"%f", &M[i][dimension]);
+        for(int i=0;i<dimension;i++){
+            fscanf(file,"%f",&M[i][dimension]);
         }
-        
 
         //Confirmacao da leitura da matriz
         for (int i = 0; i < dimension; i++){
             for (int j = 0; j < dimension+1; j++){
-                printf("M[%d][%d]= %f\n",i, j, M[i][j]);
+                printf("%f ", M[i][j]);
             }
+            printf("\n");
         }
+
+        X = Gauss(M, dimension);
+
+        //Impressao de X
+        printf("\nResultado: " );
+            for(int i=0; i<dimension; i++)
+        printf("\nX%d = %f\n", i+1, X[i]);
     }
 
     fclose(file);
@@ -53,24 +63,36 @@ void main(){
     Lembrar que a saida e um vetor do tamanho do numero de linhas 
 */
 
-float* Gauss(float **A, float *B, int N){
-    int k,i,j;
+float* Gauss(float **A, int N){
+    int k,i,j,soma;
     float M, *x;
     x=(float*)malloc(N*sizeof(float));
+
     for(k=0; k < N-1; k++){
         for(i=k+1; i<N; i++){
-            M=A[i][j]/A[k][k];
-            for(j=k; j<N; j++){
-                A[i][k]=A[i][j]-M*A[k][j];
+            M=A[i][k]/A[k][k];
+            for(j=k; j<=N; j++){
+                A[i][j]=A[i][j]-M*A[k][j];
             }
-            B[i]=B[i]-M*B[k];
         }
     }
-    for(i=N-1; i>=0; i--){
-        for(j=i+1; j<N; j++){
-            B[i] = B[i]-x[j]*A[i][j];
+    x[N-1] = A[N-1][N]/A[N-1][N-1];
+
+    //Test
+    printf("\n");
+    for(i = 0; i<N; i++){
+        for(j = 0; j<=N; j++){
+            printf("|%.3f\t", A[i][j]);
         }
-        x[i]=B[i]/A[i][i];
+        printf("\n");
+    }
+
+    for(i=N-2; i>=0; i--){
+        soma = 0;
+        for(j=i+1; j<N; j++){
+            soma = soma + x[j]*A[i][j];
+        }
+        x[i]=(A[i][N]-soma)/A[i][i];
     }
     return x;
 }
